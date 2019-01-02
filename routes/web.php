62f0db9ed17cb781d2ca39127c9ceefdc0a9c17a
vagrant <vagrant@homestead>
2019-01-02 02:26:01 +0000

    Finish basic page layout

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PagesController@root')->name('root');
 
-Route::get('/', function () {
-    return view('welcome');
-});
