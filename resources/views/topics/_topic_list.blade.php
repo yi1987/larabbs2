0c2e503db3c2f40b39d689b447f27b676a1e1e7d
vagrant <vagrant@homestead>
2019-01-03 06:31:02 +0000

    Topic order

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -9,7 +9,12 @@
                     </a>
                 </div>
 
-                <div class="media-body">
+                <div class="media-body meta">
+
+                    <a href="{{ route('categories.show', $topic->category->id) }}" title="{{ $topic->category->name }}">
+                        <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+                         {{ $topic->category->name }}
+                    </a>
 
                     <div class="media-heading">
                         <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
